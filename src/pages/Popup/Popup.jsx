906eb0ad2ddc8a9c09cdda05dec9881843b5647c
praveen_akrity <praveen.kumar@akrity.com>
2022-08-08 14:47:06 +0530

    final pages and components of chrome ext

---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from '../../assets/img/logo.svg';
+import Greetings from '../../containers/Greetings/Greetings';
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to Zluri...</h1>
+    </div>
+  );
+};
+
+export default Popup;
